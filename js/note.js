a20d02d2785714fd4520d2e7d6605b49eb371048
Danny <thanhan1100@gmail.com>
2024-12-23 19:38:46 +0700

    Note: add enlarge toggle sidebar button

--- js/note.js
+++ js/note.js
@@ -14,7 +14,8 @@ fetch("app/note.html")
             header_action: header_action
         } = windowElement(noteWindow);
 
-        // element.style.display = "flex"
+        // HEY
+        element.style.display = "flex"
 
         // Add open and close button
         const btnOpen = btnOpenAndClose(element, header_action);
@@ -22,10 +23,49 @@ fetch("app/note.html")
         // Add Drag Element function
         dragElement(element, header); // Windows
         dragElement(btnOpen); // Icon
+
+        // Initialize Note Application Logic
+        const noteMain = document.querySelector(".Note_main");
+
+        // Component
+        const containerGrid = noteMain.querySelector(".container_grid")
+        const noteTitle = noteMain.querySelector(".noteTitle");
+
+
+        // Data
+        let notesContent = [
+            {
+                noteId: 1,
+                title: "Testing 1",
+                date: "12/23/2024",
+                contentURL: `../data_storage/note/noteId_1.html`
+            },
+            {
+                noteId: 2,
+                title: "Testing 2",
+                date: "12/24/2024",
+                contentURL: `../data_storage/note/noteId_2.html`
+            }
+        ];
+
+        // Sidebar toggle
+        const toggleSidebar = document.querySelector("#toggle_sidebar");
+        
+        toggleSidebar.addEventListener("click", () => {
+            const isVisible = getComputedStyle(noteTitle).overflowY !== 'hidden';
+
+            if (isVisible) {
+                noteTitle.style.overflowX = "hidden";
+                noteTitle.style.overflowY = "hidden";
+                containerGrid.style.gridTemplateColumns = '0fr 1fr';
+                containerGrid.style.columnGap = "0";
+            } else {
+                noteTitle.style.overflowX = "hidden"
+                noteTitle.style.overflowY = "scroll"
+                containerGrid.style.gridTemplateColumns = '1fr 3fr';
+                containerGrid.style.columnGap = "10px";
+            }
+        })
     })
     .catch(error => console.error('Error loading introduction:', error));
 
-
-document.addEventListener("DOMContentLoaded", function () {
-
-})
